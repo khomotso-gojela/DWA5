2314e7dddefecec0a5288d059eddc06bec4b5483
khomotso gojela <motsoak49@gmail.com>
2023-10-04 23:25:34 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+
+  const { dividend, divider } = Object.fromEntries(entries);
+	
+	try {
+		if (!dividend || !divider) throw new Error('Division not performed. Both values are required in inputs. Try again');
+
+		if (divider < 0) throw new Error('Division not performed. Invalid number provided. Try again');
+
+		result.innerText = Math.floor(dividend / divider);
+
+	} catch (error) {
+		result.innerText = error
+	}
+
+	if (typeof(dividend) == 'string' || typeof(divider) == 'string' ) throw new Error('Something went wrong')
+});
+
+window.addEventListener('error',()=>{
+	document.body.innerText = 'Something critical went wrong. Please reload the page'
+})
